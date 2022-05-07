9d2ceae445066d32d3d928eeacd9d852c5a2645c
Akmal <magchuz@pm.me>
2022-05-07 20:30:51 +0700

    Initial commit

---
+++ instafix.py
@@ -0,0 +1,105 @@
+import json
+import re
+from http.cookiejar import MozillaCookieJar
+
+import requests
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse, RedirectResponse, StreamingResponse
+from fastapi.templating import Jinja2Templates
+from lxml import html
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+media_dict = {}
+cookies = MozillaCookieJar("cookies.txt")
+cookies.load()
+
+CRAWLER_UA = set(
+    [
+        "facebookexternalhit/1.1",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/601.2.4 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.4 facebookexternalhit/1.1 Facebot Twitterbot/1.0",
+        "facebookexternalhit/1.1",
+        "Slackbot-LinkExpanding 1.0 (+https://api.slack.com/robots)",
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:38.0) Gecko/20100101 Firefox/38.0",
+        "Mozilla/5.0 (compatible; Discordbot/2.0; +https://discordapp.com)",
+        "TelegramBot (like TwitterBot)",
+        "Mozilla/5.0 (compatible; January/1.0; +https://gitlab.insrt.uk/revolt/january)",
+    ]
+)
+
+headers = {
+    "authority": "www.instagram.com",
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-language": "en-US,en;q=0.9",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
+    "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="100"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"macOS"',
+    "sec-fetch-dest": "document",
+    "sec-fetch-mode": "navigate",
+    "sec-fetch-site": "none",
+    "sec-fetch-user": "?1",
+    "upgrade-insecure-requests": "1",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36",
+}
+
+
+def get_data(url):
+    r = requests.get(url, headers=headers, cookies=cookies)
+    tree = html.fromstring(r.text)
+    for script in tree.xpath("//script"):
+        text = script.text or ""
+        if "device_timestamp" not in text:
+            continue
+        # Remove window.__additionalDataLoaded
+        data = re.sub(r"window.__additionalDataLoaded\('/p/.*/',", "", text)
+        # Remove trailing ');'
+        data = re.sub(r"\);\s*$", "", data)
+    data = json.loads(data)
+    return data
+
+
+@app.get("/p/{post_id}", response_class=HTMLResponse)
+async def read_item(request: Request, post_id: str):
+    post_url = f"https://instagram.com/p/{post_id}"
+    if request.headers.get("User-Agent") not in CRAWLER_UA:
+        return RedirectResponse(post_url, status_code=302)
+    data = get_data(post_url)
+
+    item = data["items"][0]
+    media = item["carousel_media"][0] if "carousel_media" in item else item
+    image_url = media["image_versions2"]["candidates"][0]["url"]
+    description = media["caption"]["text"]
+
+    full_name = item["user"]["full_name"]
+    username = item["user"]["username"]
+    media_dict[post_id] = media
+
+    ctx = {
+        "request": request,
+        "url": post_url,
+        "description": description,
+        "full_name": full_name,
+        "username": username,
+    }
+
+    if "video_versions" in media:
+        ctx["video"] = f"/videos/{post_id}"
+    else:
+        ctx["image"] = image_url
+    return templates.TemplateResponse("base.html", ctx)
+
+
+@app.get("/videos/{post_id}")
+def videos(request: Request, post_id: str):
+    media = media_dict[post_id]
+    video_urls = media["video_versions"][-1]["url"]
+    if request.headers.get("User-Agent") not in CRAWLER_UA:
+        return RedirectResponse(video_urls, status_code=302)
+    return StreamingResponse(
+        requests.get(video_urls, stream=True).iter_content(chunk_size=1024),
+        media_type="video/mp4",
+        headers={"Content-Disposition": f"inline; filename={post_id}.mp4"},
+    )
