ce6c9073d7e5cf1d88c88db10e9c7cc3451dfff0
Akmal <magchuz@pm.me>
2022-10-13 13:00:43 +0700

    Get new headers

--- instafix.py
+++ instafix.py
@@ -29,21 +29,12 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 headers = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept": "*/*",
     "accept-language": "en-US,en;q=0.9",
-    "cache-control": "max-age=0",
-    "sec-ch-prefers-color-scheme": "dark",
-    "sec-ch-ua": '" Not A;Brand";v="99", "Chromium";v="100"',
-    "sec-ch-ua-mobile": "?1",
-    "sec-ch-ua-platform": '"Android"',
-    "sec-fetch-dest": "document",
-    "sec-fetch-mode": "navigate",
-    "sec-fetch-site": "none",
-    "sec-fetch-user": "?1",
-    "upgrade-insecure-requests": "1",
+    "cache-control": "no-cache",
+    "pragma": "no-cache",
     "referer": "https://www.instagram.com/",
-    "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Mobile Safari/537.36",
-    "viewport-width": "1280",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36",
 }
 
 
