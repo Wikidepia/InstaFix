6d26ef85cbdd3d6759476fe15fa85cfa305d56c2
Akmal <magchuz@pm.me>
2022-11-17 10:14:38 +0700

    Add sec-fetch-mode to headers
    dunno why ig need this

--- instafix.py
+++ instafix.py
@@ -34,6 +34,7 @@ headers = {
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "accept-language": "en-US,en;q=0.9",
     "cache-control": "max-age=0",
+    "sec-fetch-mode": "navigate",
     "upgrade-insecure-requests": "1",
     "referer": "https://www.instagram.com/",
     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.60 Safari/537.36",
